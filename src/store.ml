3dcccd6881f017f3d77ea811226f0cb64f695dc4
Adrien DE SEDE <adrien.de.sede@gmail.com>
2024-10-01 05:30:36 +0200

    fix: remove store
    bad approach, will cause issues when working on scopes

--- src/store.ml
+++
@@ -1,6 +0,0 @@
-let store = ref (Hashtbl.create 1000)
-
-let set key value =
-  match Hashtbl.find_opt !store key with
-  | None -> Hashtbl.add !store key value
-  | Some _ -> Hashtbl.replace !store key value
